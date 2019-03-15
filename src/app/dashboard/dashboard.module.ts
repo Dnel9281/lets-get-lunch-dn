e2d610680ef99b1e75553eaecb2d24dc2865c037
David Nelson <nelsondavidmark@gmail.com>
2019-03-15 14:28:58 +0700

    Add calendar to dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    CalendarModule.forRoot({provide: DateAdapter,  useFactory: adapterFactory})
   ]
 })
 export class DashboardModule { }
